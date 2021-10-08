3c6f03f61c9ab448a07656f379df1165bc8a9ecc
Matias Nassi <matinas88@gmail.com>
2021-10-07 22:07:12 -0300

    Initial logic for the button presses and workout animations

--- scripts/GameLogic.js
+++ scripts/GameLogic.js
@@ -9,7 +9,37 @@ export const Diagnostics = require('Diagnostics');
 const DOOR_OPENING_MAX_ROTATION = -170
 const DOOR_OPENING_DURATION = 5000;
 const PI = 3.1416;
-const STATE = { NotStarted: 0, OpeningDoors: 1, DoorsOpened: 2, WorkingOut: 3, WorkoutCompleted: 4, DoorsClosed: 5 };
+const STATE = { NotStarted: 0, OpeningDoors: 1, DoorsOpened: 2, ButtonSelect : 3, WorkingOut_Burpees: 4, WorkingOut_Pushups: 5, WorkingOut_Situps: 6, WorkoutCompleted: 7, AllWorkoutsCompleted : 8, DoorsClosed: 9 };
+
+const clipsMapping = {
+  "Idle": "mixamo.com",
+  "IdleToPushup": "mixamo.com0",
+  "Pushup": "mixamo.com1",
+  "PushupToIdle": "mixamo.com2",
+  "BurpeeStart": "mixamo.com3",
+  "Burpee": "mixamo.com4",
+  "BurpeeEnd": "mixamo.com5",
+  "IdleToSitup": "mixamo.com9",
+  "Situp": "mixamo.com7",
+  "SitupToIdle": "mixamo.com8",
+};
+
+const burpeesAnimationSequence = [
+  "BurpeeStart", "Burpee", "Burpee", "BurpeeEnd",
+]
+
+const pushupsAnimationSequence = [
+  "IdleToPushup", "Pushup", "Pushup", "PushupToIdle",
+]
+
+const situpsAnimationSequence = [
+  "IdleToSitup", "Situp", "Situp", "Situp", "SitupToIdle",
+]
+
+let currentAnimationSequence;
+let currentClipIndex = 0;
+let playbackController;
+let isPlaying;
 
 var state = R.val(STATE.NotStarted);
 
@@ -25,6 +55,12 @@ Promise.all(
     S.root.findFirst('leftTopDoor'),
     S.root.findFirst('buttonCap'),
     P.outputs.getBoolean("doorsOpened"),
+    S.root.findFirst('nodes_5_burpees'),
+    S.root.findFirst('nodes_5_pushups'),
+    S.root.findFirst('nodes_5_situps'),
+    P.outputs.getBoolean("buttonsReady"),
+    S.root.findFirst('Miguelito'),
+    A.playbackControllers.findFirst('animationPlaybackController0')
   ]
 ).then(main).catch((error) =>
   {
@@ -49,8 +85,14 @@ async function main(assets) { // Enables async/await in JS [part 1]
   const rightTopDoor = assets[5];
   const topDoor = assets[6];
   const leftTopDoor = assets[7];
-  const button = assets[8];
+  const mainButton = assets[8];
   const doorsOpened = assets[9];
+  const burpeesButton = assets[10];
+  const pushupsButton = assets[11];
+  const situpsButton = assets[12];
+  const buttonsReady = assets[13];
+  const model = assets[14];
+  playbackController = assets[15];
 
   Diagnostics.log("All assets loaded");
 
@@ -67,12 +109,82 @@ async function main(assets) { // Enables async/await in JS [part 1]
   topDoor.transform.rotationY = R.clamp(R.sum(doorOpenSignal,0.7), DOOR_OPENING_MAX_ROTATION, 0);
   leftTopDoor.transform.rotationY = R.clamp(R.sum(doorOpenSignal,0.8), DOOR_OPENING_MAX_ROTATION, 0);
 
-  TG.onTap(button).subscribe(() => 
+  playbackController.looping = R.val(true);
+  isPlaying = playbackController.playing;
+
+  isPlaying.monitor().subscribe(OnAnimationFinished);
+
+  TG.onTap(mainButton).subscribe(() => 
   {
     state = R.val(STATE.OpeningDoors);
     P.inputs.setScalar("state", state);
 
     doorOpenDriver.start();
+  });
+
+  TG.onTap(burpeesButton).subscribe(async () => 
+  {
+    if (state.pinLastValue() == STATE.ButtonSelect)
+    {
+      Diagnostics.log("Burpees button pressed!");
+
+      currentClipIndex = 0;
+      currentAnimationSequence = burpeesAnimationSequence;
+      AdvanceAnimationPlayback();
+
+      state = R.val(STATE.WorkingOut_Burpees);
+      P.inputs.setScalar("state", state);
+    }
+    else
+    {
+      Diagnostics.log("ERROR: Button console still not ready!");
+    }
+  });
+
+  TG.onTap(pushupsButton).subscribe(async () => 
+  {
+    if (state.pinLastValue() == STATE.ButtonSelect)
+    {
+      Diagnostics.log("Pushups button pressed!");
+
+      currentClipIndex = 0;
+      currentAnimationSequence = pushupsAnimationSequence;
+      AdvanceAnimationPlayback();
+
+      state = R.val(STATE.WorkingOut_Pushups);
+      P.inputs.setScalar("state", state);
+    }
+    else
+    {
+      Diagnostics.log("ERROR: Button console still not ready!");
+    }
+  });
+
+  TG.onTap(situpsButton).subscribe(() => 
+  {
+    if (state.pinLastValue() == STATE.ButtonSelect)
+    {
+      Diagnostics.log("Situps button pressed!");
+      
+      currentClipIndex = 0;
+      currentAnimationSequence = situpsAnimationSequence;
+      AdvanceAnimationPlayback();
+
+      state = R.val(STATE.WorkingOut_Situps);
+      P.inputs.setScalar("state", state);
+    }
+    else
+    {
+      Diagnostics.log("ERROR: Button console still not ready!");
+    }
+  });
+
+  buttonsReady.monitor().subscribe(() =>
+  {
+    Diagnostics.log("Ready to select buttons");
+
+    state = R.val(STATE.ButtonSelect);
+    P.inputs.setScalar("state", state);
   });
 
   doorsOpened.monitor().subscribe(() =>
@@ -91,4 +203,41 @@ function outputToPatch()
 {
   P.inputs.setScalar("state", state);
   Diagnostics.watch("State: ", state);
+}
+
+async function AdvanceAnimationPlayback()
+{
+  var animation = currentAnimationSequence[currentClipIndex];
+  Diagnostics.log("Animation: " + animation);
+  var clipName = clipsMapping[animation];
+  Diagnostics.log("ClipName: " + clipName);
+  var clip = await A.animationClips.findFirst(clipName);
+  Diagnostics.log("Clip: " + clip);
+
+  playbackController.looping = R.val(false);
+  playbackController.playing = R.val(true);
+  playbackController.setAnimationClip(clip);
+}
+
+async function OnAnimationFinished()
+{
+  Diagnostics.log("Animation finished!");
+
+  currentClipIndex++;
+  if (currentClipIndex < currentAnimationSequence.length)
+  {
+    AdvanceAnimationPlayback();
+  }
+  else
+  {
+    Diagnostics.log("Workout completed, select another one");
+
+    var clip = await A.animationClips.findFirst(clipsMapping["Idle"]);
+    playbackController.looping = R.val(true);
+    playbackController.playing = R.val(true);
+    playbackController.setAnimationClip(clip);
+
+    state = R.val(STATE.ButtonSelect);
+    P.inputs.setScalar("state", state);
+  }
 }
